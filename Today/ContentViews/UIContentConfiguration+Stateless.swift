efebf0af09d1f0910651d9431b2283169acc3da6
Huang Wei <huangwei.robin@gmail.com>
2022-05-17 12:13:48 +0800

    Using ContentViews

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by HuangWei on 2022/5/17.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
